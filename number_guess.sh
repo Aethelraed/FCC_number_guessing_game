5bea7730170f7c2520c601e4aac6b30654bafe9d
Aethelraed the Unready <aethelraed@tutanota.com>
2024-06-11 04:24:58 +0000

    feat: added welcome back and user registration prompts and logic

--- number_guess.sh
+++ number_guess.sh
@@ -1,8 +1,29 @@
 #!/bin/bash
 #a number guessing game
-
+PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
 SECRET_RN=$(( $RANDOM%1000 ))
 echo $SECRET_RN
 
+WELCOME_BACK(){
+  N_GAMES=$($PSQL "select count(game_id) from games where user_id=$USER_ID;")
+  BEST_N=$($PSQL "select min(n_guesses) from games where user_id=$USER_ID;")
+  echo "Welcome back, $USER_NAME! You have played $N_GAMES games, and your best game took $BEST_N guesses."
+}
+
+USER_REGISTRATION(){
+  echo "Welcome, $USER_NAME! It looks like this is your first time here."
+  REGIS_RETURN=$($PSQL "insert into users(user_name) values('$USER_NAME');")
+  USER_ID=$($PSQL "select user_id from users where user_name='$USER_NAME';")
+}
+
 echo -e "\nEnter your username:\n"
 read USER_NAME 
+
+USER_ID=$($PSQL "select user_id from users where user_name='$USER_NAME';")
+
+if [[ -z $USER_ID ]]
+then 
+  USER_REGISTRATION
+else
+  WELCOME_BACK
+fi
