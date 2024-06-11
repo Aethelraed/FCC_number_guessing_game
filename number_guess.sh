2e320ab42f61027fce7a158789a88c5e0ccb34c1
Aethelraed the Unready <aethelraed@tutanota.com>
2024-06-11 04:53:08 +0000

    feat: added game logic

--- number_guess.sh
+++ number_guess.sh
@@ -2,7 +2,8 @@
 #a number guessing game
 PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
 SECRET_RN=$(( $RANDOM%1000 ))
-echo $SECRET_RN
+
+N_GUESS=0
 
 WELCOME_BACK(){
   N_GAMES=$($PSQL "select count(game_id) from games where user_id=$USER_ID;")
@@ -16,6 +17,32 @@ USER_REGISTRATION(){
   USER_ID=$($PSQL "select user_id from users where user_name='$USER_NAME';")
 }
 
+GUESS(){
+  if [[ $1 ]]
+  then
+    echo "$1"
+  fi
+  read USER_GUESS
+  if [[ $USER_GUESS =~ [^0-9]+ ]]
+  then 
+    GUESS "That is not an integer, guess again:"
+    else
+      N_GUESS=$(( N_GUESS + 1 ))
+      if [[ $USER_GUESS > $SECRET_RN ]]
+      then
+        GUESS "It's lower than that, guess again:"
+      else 
+      if [[ $USER_GUESS < $SECRET_RN ]]
+        then        
+          GUESS "It's higher than that, guess again:"
+        else 
+        echo "You guessed it in $N_GUESS tries. The secret number was $SECRET_RN. Nice job!"
+        INSER_RETURN=$($PSQL "insert into games(n_guesses,user_id) values($N_GUESS,$USER_ID);")
+      fi 
+      fi
+  fi
+}
+
 echo -e "\nEnter your username:\n"
 read USER_NAME 
 
@@ -27,3 +54,4 @@ then
 else
   WELCOME_BACK
 fi
+GUESS "Guess the secret number between 1 and 1000:"
